d524a6c073a9ace42e09a07a3b8061a7f9b023d1
MatheusMuriel <matheus.muriel@outlook.com>
2017-09-29 16:24:34 -0300

    Cadastro e Array

---
+++ br/edu/unifil/lp1/consulta/controle/CadastroDeConsultas.java
@@ -0,0 +1,24 @@
+package br.edu.unifil.lp1.consulta.controle;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/*
+ *  Incluir uma nova Consulta no cadastro(array);
+ 
+Exibir matrícula e nome dos pacientes de todas as Consultas específico de plano de saúde:
+    Prata, Ouro, Diamante e Família.
+    
+Retornar o número total de Consultas de um tipo específico de plano de saúde: Prata, Ouro,
+    Diamante e Família.
+    
+Retornar o valor total de Consultas de um tipo específico de plano de saúde: Prata, Ouro,
+    Diamante e Família.
+ */
+
+public class CadastroDeConsultas
+{
+    public static void main(String[] args){
+        ArrayList<String> listaConsultas = new ArrayList<String> ();
+        
+    }
+}
---
+++ br/edu/unifil/lp1/consulta/visao/SistemaDaClinica.java
@@ -0,0 +1,31 @@
+package br.edu.unifil.lp1.consulta.visao;
+import java.util.Scanner;
+public class SistemaDaClinica
+{
+    public static void main(String args[]){
+        CadastroDeConsulta CdC = new CadastroDeConsulta();
+        Scanner teclado = new Scanner(System.in);
+        String nomeDoPaciente;
+        int matricula;
+        int numeroDeDependentes;
+        int anoDeInclusaoNoPlano;
+        float valorDaConsulta;
+        int opcao = 0;
+        
+        while(opcao != 10){
+            //Agendar uma consulta
+            //Deve ser criado se informado os dados do paciente
+            System.out.println("\n");
+            System.out.println("1- Nova consulta");
+            opcao = teclado.nextInt();
+            switch(opcao){
+                case 1 : System.out.print("Nome do paciente: ");
+                            nomeDoPaciente = teclado.nextLine();
+                         System.out.print("Matricula: ");
+                            matricula = teclado.nextInt();
+                         System.out.print("Numero de Dependentes: ");
+                            numeroDeDependentes = teclado.nextInt();
+            }
+        }
+    }
+}
